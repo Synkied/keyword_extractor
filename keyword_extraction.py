2821d8bdb8c775f860d5307d2601784f0e03df58
Quentin <synkx@hotmail.fr>
2020-11-14 17:09:04 +0100

    review

--- keyword_extraction.py
+++ keyword_extraction.py
@@ -27,10 +27,44 @@ class KeyWordExtractor():
         result = self.keywords_analyze(self.text_words_count)
 
         self.notable_keywords = result['notable_keywords']
-        print(self)
+        for k, v in self.notable_keywords.items():
+            string = '%s: %s' % (k, v)
+            print(string)
 
     def __repr__(self):
         return 'Notable keywords: %s' % (self.notable_keywords)
+
+    def determine_file_type(self, path):
+        """
+        Try guessing what's the file type
+        """
+        file_type_name = ''
+
+        file_type, encoding = mimetypes.guess_type(path)
+        if file_type:
+            file_type_name = file_type.split('application/')[-1]
+
+        return file_type_name
+
+    def agg_texts(self, file_type, filepath):
+        if file_type in HANDLED_FILE_TYPES.keys():
+            handler = HANDLED_FILE_TYPES[file_type]()
+            text = handler.extract_text(filepath)
+
+            return text
+
+        return {}
+
+    def clean_text(self, text):
+        text = re.sub(UNWANTED_CHARS_REGEX, ' ', str(text))
+        words = word_tokenize(text)
+        result = ' '.join(
+            [
+                word for word in words
+                if word.lower() not in self.lang_stopwords
+            ]
+        )
+        return result
 
     def text_extract(self, path):
         """
@@ -51,14 +85,14 @@ class KeyWordExtractor():
                     file_type = self.determine_file_type(filepath)
                     texts_agg.setdefault(file_type, {})
 
-                    extracted_text = self.agg_datas(file_type, filepath)
+                    extracted_text = self.agg_texts(file_type, filepath)
                     filename = os.path.split(filepath)[-1]
                     texts_agg[filename] = extracted_text
 
         elif os.path.isfile(path):
             filename = os.path.split(path)[-1]
             file_type = self.determine_file_type(path)
-            extracted_text = self.agg_datas(file_type, path)
+            extracted_text = self.agg_texts(file_type, path)
             texts_agg[filename] = extracted_text
 
         return texts_agg
@@ -88,20 +122,22 @@ class KeyWordExtractor():
             for page in keywords_extract[f]:
                 per_page_notable_keywords[f][page] = {}
                 page_words = keywords_extract[f][page]
-                counters = page_words.values()
-                page_max_counter = max(counters)
-                page_min_counter = min(counters)
-                page_mean = (page_max_counter + page_min_counter) / 2
-                means.append(page_mean)
 
-                for k, v in page_words.items():
-                    if k not in words.keys():
-                        words[k] = v
-                    else:
-                        words[k] += v
+                if page_words:
+                    counters = page_words.values()
+                    page_max_counter = max(counters)
+                    page_min_counter = min(counters)
+                    page_mean = (page_max_counter + page_min_counter) / 2
+                    means.append(page_mean)
 
-                    if v > page_mean:
-                        per_page_notable_keywords[f][page][k] = v
+                    for k, v in page_words.items():
+                        if k not in words.keys():
+                            words[k] = v
+                        else:
+                            words[k] += v
+
+                        if v > page_mean:
+                            per_page_notable_keywords[f][page][k] = v
 
         total_max = max(means)
         total_min = min(means)
@@ -125,17 +161,6 @@ class KeyWordExtractor():
         }
         return result
 
-    def clean_text(self, text):
-        text = re.sub(UNWANTED_CHARS_REGEX, ' ', text)
-        words = word_tokenize(text)
-        result = ' '.join(
-            [
-                word for word in words
-                if word.lower() not in self.lang_stopwords
-            ]
-        )
-        return result
-
     def word_counter(self, text):
         words_count = {}
         for word in word_tokenize(text):
@@ -146,27 +171,6 @@ class KeyWordExtractor():
                 words_count[word] += 1
 
         return words_count
-
-    def agg_datas(self, file_type, filepath):
-        if file_type in HANDLED_FILE_TYPES.keys():
-            handler = HANDLED_FILE_TYPES[file_type]()
-            text = handler.extract_text(filepath)
-
-            return text
-
-        return {}
-
-    def determine_file_type(self, path):
-        """
-        Try guessing what's the file type
-        """
-        file_type_name = ''
-
-        file_type, encoding = mimetypes.guess_type(path)
-        if file_type:
-            file_type_name = file_type.split('application/')[-1]
-
-        return file_type_name
 
 
 if __name__ == '__main__':
